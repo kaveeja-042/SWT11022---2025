13726d3cc79b3313e62e5495e3ee7a5e6fb40c65
kaveeja-042 <kaveejarathnakoon2002@gmail.com>
2025-02-19 09:56:36 +0530

    added new file and updated second.c file

--- first.c
+++ first.c
@@ -4,5 +4,6 @@ int main{
     printf("Welcome to the fundamental of programming session");
     printf("R.K.N. Rathnakoon");
     printf("SEU/IS/22/ICT/042");
+    printf("19-02-2025");
     return 0;
 }
